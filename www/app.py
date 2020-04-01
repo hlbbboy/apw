6385aa5a22480d5ee2196abed890665efc94a604
hlbbboy <hanliang_1001@126.com>
2020-04-01 22:15:12 +0800

    add manage_blog_edit.html and update related files like apis, handlers

--- www/app.py
+++ www/app.py
@@ -75,7 +75,7 @@ async def response_factory(app, handler):
                 resp.content_type = 'application/json;charset=utf-8' #application/json是一个json对象文件
                 return resp
             else:
-                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8')) #render方法是jinja2用来将变量传递给template的
                 resp.content_type = 'text/html;charset=utf-8'
                 return resp
         if isinstance(r, int) and r >= 100 and r < 600:
